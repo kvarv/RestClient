5f1b1ca79edc2e01085700922dd2d9daf79e7380
Gøran Sveia Kvarv <goran.kvarv@gmail.com>
2014-10-19 23:59:00 +0200

    initial commit

---
+++ src/RestClient.Tests/Tests.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace Rest.Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public async Task Should()
+        {
+            var restClient = new RestClient("");
+
+            var httpResponseMessage = await restClient.HttpClient.GetAsync("");
+            
+        }
+    }
+}
---
+++ src/RestClient/RestClient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+
+namespace Rest
+{
+    public class RestClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public RestClient(string url)
+            : this(new HttpClient {BaseAddress = new Uri(url)})
+        {
+        }
+
+        public RestClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public HttpClient HttpClient
+        {
+            get { return _httpClient; }
+        }
+    }
+}
